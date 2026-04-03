744eafd4257175e999e097f9bf4a1fe5208dff6b
y9san9 / Alex Sokol <y9san9@gmail.com>
2026-04-03 13:50:34 +0300

    fix(tsx): closing elements might be other than identifier

--- after/queries/tsx/highlights.scm
+++ after/queries/tsx/highlights.scm
@@ -9,12 +9,12 @@
 ] @y9nika.muted
 
 (jsx_closing_element
-  name: (identifier) @y9nika.muted)
+  name: (_) @y9nika.muted)
 
 (jsx_opening_element
-  name: (identifier) @y9nika.muted)
+  name: (_) @y9nika.muted)
 
 (jsx_self_closing_element
-  name: (identifier) @y9nika.muted)
+  name: (_) @y9nika.muted)
 
 (jsx_text) @y9nika.variable
