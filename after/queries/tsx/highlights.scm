15e12e8669268ade38095babfa6894779da34542
y9san9 / Alex Sokol <y9san9@gmail.com>
2026-04-03 13:59:46 +0300

    fix(tsx): make priority of y9nika higher than the one of jsx_element

--- after/queries/tsx/highlights.scm
+++ after/queries/tsx/highlights.scm
@@ -9,12 +9,12 @@
 ] @y9nika.muted
 
 (jsx_closing_element
-  name: (_) @y9nika.muted)
+  name: (_) @y9nika.muted (#set! "priority" 110))
 
 (jsx_opening_element
-  name: (_) @y9nika.muted)
+  name: (_) @y9nika.muted (#set! "priority" 110))
 
 (jsx_self_closing_element
-  name: (_) @y9nika.muted)
+  name: (_) @y9nika.muted (#set! "priority" 110))
 
 (jsx_text) @y9nika.variable
