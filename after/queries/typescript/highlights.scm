00c5530f3efa7dc865dcb35dfa50af894676ec64
y9san9 / Alex Sokol <y9san9@gmail.com>
2026-04-10 14:54:05 +0300

    feat(typescript): added more highlighting of declarations

--- after/queries/typescript/highlights.scm
+++ after/queries/typescript/highlights.scm
@@ -1,21 +1,37 @@
 ;; vim: ft=query
 ;; extends
 
+;; Declarations
+
 (interface_declaration
   name: (type_identifier) @y9nika.declaration)
 
-(method_definition
-  name: (property_identifier) @y9nika.declaration)
+(class_declaration
+  name: (type_identifier) @y9nika.declaration)
+
+(type_alias_declaration
+  name: (type_identifier) @y9nika.declaration)
+
+(enum_declaration
+  name: (identifier) @y9nika.declaration)
 
 (function_declaration
   name: (identifier) @y9nika.declaration)
 
+(method_definition
+  name: (property_identifier) @y9nika.declaration)
+
+(method_signature
+  name: (property_identifier) @y9nika.declaration)
+
 (variable_declarator
   name: (identifier) @y9nika.declaration
-  value: (arrow_function))
+  value: [
+    (arrow_function)
+    (function_expression)
+  ])
 
-(type_alias_declaration
-  name: (type_identifier) @y9nika.declaration)
+;; Variables
 
 (variable_declarator
   name: (identifier) @y9nika.variable)
@@ -26,15 +42,27 @@
 
 (variable_declarator
   name: (object_pattern
-    (shorthand_property_identifier_pattern) @y9nika.variable))
-
-(variable_declarator
-  name: (object_pattern
-    (pair_pattern
-      key: (property_identifier) @y9nika.variable)))
+    [
+      (shorthand_property_identifier_pattern)
+      (pair_pattern
+        key: (property_identifier))
+    ] @y9nika.variable))
 
 (property_signature
-    name: (property_identifier) @y9nika.variable)
+  name: (property_identifier) @y9nika.variable)
+
+(public_field_definition
+  name: (property_identifier) @y9nika.variable)
+
+(method_signature
+  parameters: (formal_parameters
+    (required_parameter
+      pattern: (identifier) @y9nika.variable)))
+
+(method_signature
+  parameters: (formal_parameters
+    (optional_parameter
+      pattern: (identifier) @y9nika.variable)))
 
 [
   (null)
